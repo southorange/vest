465c703e4f049170dbc3b3e68b8b04be647431bc
southorange <southorange@users.noreply.github.com>
2016-05-26 17:10:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,10 +59,10 @@
   }
 
   /**
-   * Duo flag.
+   *
    */
 
-  require.duo = true;
+  require.duo = false;
 
   /**
    * Expose cache.
@@ -92,13 +92,7 @@ var Highlight = require('segmentio/highlight');
 /**
  * Code highlighting.
  */
-
-new Highlight()
-  .use(require('segmentio/highlight-javascript'))
-  .use(require('segmentio/highlight-xml'))
-  .use(require('segmentio/highlight-css'))
-  .use(require('segmentio/highlight-json'))
-  .all();
+//This isn't a server
 
 
   
